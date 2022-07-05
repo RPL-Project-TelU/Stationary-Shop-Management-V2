bdc8ca0173dc65bc9483dc55968ac571e0ebb794
Ahmadnaufal2525 <ahmadnaufal588@gmail.com>
2022-07-05 22:50:50 +0700

    update unit testing

--- src/pages/SalesEntry/SalesEntry.test.js
+++ src/pages/SalesEntry/SalesEntry.test.js
@@ -13,21 +13,40 @@ describe ("Test Sale Entry Components", () => {
   test("purchase field should accept Pembelian",async () => {
     render(<SalesEntry/>);
     const purchase = await screen.findByPlaceholderText("Pembelian");
-    userEvent.type(purchase, "5 bk")
+    userEvent.type(purchase, "5 buku")
     expect(purchase.value).not.toMatch("5 Buku");
+  });
+  test("purchase field should accept Pembelian",async () => {
+    render(<SalesEntry/>);
+    const purchase = await screen.findByPlaceholderText("Pembelian");
+    userEvent.type(purchase, "5 Buku")
+    expect(purchase.value).toMatch("5 Buku");
   });
   test("price field should accept Harga",async () => {
     render(<SalesEntry/>);
     const price = await screen.findByPlaceholderText("Harga");
     console.log(price);
-    userEvent.type(price, "lima puluh ribu")
-    expect(price.value).not.toMatch("50000");
+    userEvent.type(price, "50000")
+    expect(price.value).not.toMatch("50.000");
+  });
+  test("price field should accept Harga",async () => {
+    render(<SalesEntry/>);
+    const price = await screen.findByPlaceholderText("Harga");
+    console.log(price);
+    userEvent.type(price, "50.000")
+    expect(price.value).toMatch("50.000");
   });
   test("payment field should accept Pembayaran",async () => {
     render(<SalesEntry/>);
     const payment = await screen.findByPlaceholderText("Pembayaran");
-    userEvent.type(payment, "seribu")
-    expect(payment.value).not.toMatch("1000");
+    userEvent.type(payment, "1000")
+    expect(payment.value).not.toMatch("1.000");
+  });
+  test("payment field should accept Pembayaran",async () => {
+    render(<SalesEntry/>);
+    const payment = await screen.findByPlaceholderText("Pembayaran");
+    userEvent.type(payment, "1.000")
+    expect(payment.value).toMatch("1.000");
   });
   test('calls onClick prop when clicked', () => {
     const handleClick = jest.fn();
